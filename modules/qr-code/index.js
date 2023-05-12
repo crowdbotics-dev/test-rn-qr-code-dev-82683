17385372a20055d4389c88a3274db42832ad1efb
abdelhak@crowdbotics.com <abdelhak@crowdbotics.com>
2023-05-12 15:34:27 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ modules/qr-code/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navigator from "./Navigator";
+
+const QRCode = props => {
+  return <Navigator />;
+};
+
+export default {
+  title: "QRCode",
+  navigator: QRCode
+};
