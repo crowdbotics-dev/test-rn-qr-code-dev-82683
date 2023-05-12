88989b452af1e330cc10185ad78f80d00be2ed1e
abdelhak@crowdbotics.com <abdelhak@crowdbotics.com>
2023-05-12 14:11:00 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "test-rn-qr-code-dev-82683",
-  url: "https://test-rn-qr-code-dev-82683.botics.co",
-  api: "https://test-rn-qr-code-dev-82683.botics.co/api/v1"
-}
+  "name": "test-rn-qr-code-dev-82683",
+  "url": "https://test-rn-qr-code-dev-82683.botics.co",
+  "api": "https://test-rn-qr-code-dev-82683.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
